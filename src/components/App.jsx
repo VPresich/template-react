1a3dd085e68d5c078e251a405682af066d4754a5
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-18 13:54:30 +0200

    Add components from lesson 1-1

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Product from "./Product/Product";
+
+function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+export default App;

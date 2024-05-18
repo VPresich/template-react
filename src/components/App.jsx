b20b4f0769443a34ce31b26a4b417189c75b6669
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-18 14:44:18 +0200

    Create component with props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,17 @@ function App() {
     <div>
       <h1>Best selling</h1>
 
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={9.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <Product name="Fries and Burger" price={15.29} />
     </div>
   );
 }
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,12 @@
-const Product = () => {
-  const price = 999;
-  const imgUrl =
-    "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => {
   return (
     <div>
-      <h2>Tacos</h2>
-      <img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="640" />
       <p>Price: {price} credits</p>
     </div>
   );

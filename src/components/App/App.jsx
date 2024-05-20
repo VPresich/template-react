0b7484a0027143af4ebd2349355734a859a1582a
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-20 23:30:07 +0200

    Examples of Track Clickers

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,6 @@
-import { useState } from "react";
-import TrackClicker from "../TrackClicker/TrackClicker";
-import IsolatedTrackClicker from "../IsolatedTrackClicker/IsolatedTrackClicker";
-import css from "./App.module.css";
-const data = [1, 2, 3, 4, 5];
+import Clickers from "../Clickers/Clickers";
+
 function App() {
-  const [clicks, setClicks] = useState(0);
-
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-
-  return (
-    <div className={css.maplist}>
-      {data.map((item, index) => (
-        <TrackClicker key={index} value={clicks} onClicker={handleClick}>
-          Button{item}
-        </TrackClicker>
-      ))}
-
-      <IsolatedTrackClicker>IzButton</IsolatedTrackClicker>
-      <IsolatedTrackClicker>izButton</IsolatedTrackClicker>
-    </div>
-  );
+  return <Clickers>Examples Track Clickers</Clickers>;
 }
 export default App;

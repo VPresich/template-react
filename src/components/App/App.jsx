035368302b93b45f635715c12597596863d6375d
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-27 01:06:13 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
 import Balance from "../Balance/Balance";
 import LangSwitcher from "../LangSwitcher/LangSwitcher";
+import { selectLang } from "../redux/localeSlice";
 // import css from "./App.module.css";
 
 const App = () => {
+  const lang = useSelector(selectLang);
   return (
     <>
       <Balance />
       <LangSwitcher />
+      <p>Selected lang: {lang}</p>
     </>
   );
 };

e656273cce99d0d40f041ce8015b7c3dac410f85
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-27 14:37:46 +0200

    update local slice

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,18 @@
 import { useSelector } from "react-redux";
 import Balance from "../Balance/Balance";
 import LangSwitcher from "../LangSwitcher/LangSwitcher";
-import { selectLang } from "../redux/localeSlice";
+import { selectLang, selectUpdated } from "../redux/localeSlice";
 // import css from "./App.module.css";
 
 const App = () => {
   const lang = useSelector(selectLang);
+  const lastUpdate = useSelector(selectUpdated);
   return (
     <>
       <Balance />
       <LangSwitcher />
       <p>Selected lang: {lang}</p>
+      <p>Last update: {lastUpdate}</p>
     </>
   );
 };
--- src/components/redux/localeSlice.js
+++ src/components/redux/localeSlice.js
@@ -1,13 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const selectLang = (state) => state.locale.lang;
+export const selectUpdated = (state) => state.locale.updatedAt;
 
 const slice = createSlice({
   name: "locale",
-  initialState: { lang: "uk" },
+  initialState: { lang: "uk", updatedAt: Date.now() },
   reducers: {
-    changeLang(state, action) {
-      state.lang = action.payload;
+    changeLang: {
+      reducer: (state, action) => {
+        state.lang = action.payload.value;
+        state.updatedAt = action.payload.time;
+      },
+      prepare: (value) => {
+        return {
+          payload: {
+            value,
+            time: Date.now(),
+          },
+        };
+      },
     },
   },
 });
